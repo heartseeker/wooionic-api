135c5aff88d645f5a7f72e6821e3b33be3e3e1a9
Alex Strauss <alexinformationtech@gmail.com>
2018-01-07 21:59:31 +0800

    update headers

--- api/routes.js
+++ api/routes.js
@@ -31,19 +31,6 @@ router.get('/orders', function (req, res) {
 
 router.get('/products', function (req, res) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
     var url_parts = url.parse(req.url, true);
     var query = url_parts.search;
 
